25929c212dc5797f96543c8c17343eeb548a3514
nicjlambert <lambni85@outlook.com>
2021-06-20 23:03:20 +1000

    added JSX example

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import * as React from 'react';
 // ie definte title only once and not
 // everytime the function is called
 // a variable declared with const cannot be re-assigned
-const title = 'React'
+
+const welcome = {
+greeting:'Hey',
+title:'React'
+
+};
 
 function App(){ 
 // you can do somethig here between
@@ -14,7 +19,11 @@ function App(){
 
   return(
     <div>
-     <h1>Hello world!</h1>
+     <h1>{welcome.greeting} {welcome.title}!</h1>
+
+     <label htmlFor="search">Search:</label>
+     <input id="search" type="text"/>
+
     </div>
   );
 }
