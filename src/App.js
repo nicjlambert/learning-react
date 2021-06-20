349258ee3f66df51719b46eaf60402c0e5c6adcf
nicjlambert <lambni85@outlook.com>
2021-06-20 22:39:08 +1000

    simplified App.js, added const Title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// ie definte title only once and not
+// everytime the function is called
+// a variable declared with const cannot be re-assigned
+const title = 'React'
+
+function App(){ 
+// you can do somethig here between
+// the function signature and the return
+// statement
+
+
+
+  return(
+    <div>
+     <h1>Hello world!</h1>
     </div>
   );
 }
